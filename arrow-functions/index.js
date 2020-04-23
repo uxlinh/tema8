cac2874b6c1ca4d2382930750565c2e14663809e
uxlinh <linh.tn94@gmail.com>
2020-04-23 09:56:07 +0200

    js

---
+++ arrow-functions/index.js
@@ -0,0 +1,12 @@
+console.log('i am here')
+
+function square(tall, name) {
+    return name + ', regnestykket ditt gir:' + tall * tall
+}
+
+console.log (square(16, 'Per'))
+
+const squareA = (tall) => tall * tall
+
+
+console.log(squareA(17))
