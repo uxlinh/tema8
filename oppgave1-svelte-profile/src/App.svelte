e08828869208598c28a00b2d1aacef23df18053b
uxlinh <linh.tn94@gmail.com>
2020-06-01 09:59:24 +0200

    ..

--- oppgave1-svelte-profile/src/App.svelte
+++ oppgave1-svelte-profile/src/App.svelte
@@ -1,11 +1,46 @@
 <script>
-	export let name;
+	let forrett  
+	let hovedrett
+	let dessert 
+	let step = 'registrer'
 </script>
+	
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{#if step == 'registrer' }
+	<h1>Registrer deg som bruker</h1>
+	<label>Forrett</label>
+	<input placeholder="Skriv inn forrett" type="text" bind:value={forrett}> 
+	<br>
+	<span>Hovedrett</span>
+	<input placeholder="Skriv inn hovedrett" type="text" bind:value={hovedrett}>
+	<br>
+	<span>Dessert</span>
+	<input placeholder="Skriv inn Dessert" type="text" bind:value={dessert}>
+	
+	<button on:click={ ()=> { step='kvittering' } }>Registrer</button>	
+	{/if}
+	
+	 
+	{#if step == 'kvittering' }
+	<h4>Din bestilling:</h4>
+	<ol> 
+		<li>Forrett: {forrett}</li>
+		<li>Hovedrett: {hovedrett}</li>
+		<li>Dessert: {dessert}</li>
+	</ol>
+	<button on:click={ ()=> { step='registrer' } }>Kanseller Registrering</button>	
+	<button on:click={ ()=> { step='registrer' } }>Tilbake</button>	
+	<button on:click={ ()=> { step='bekreftet' } }>Bekreft</button>	
+	{/if}
+	
+	{#if step == 'bekreftet' }
+	<h4>Bekreftet</h4>
+	{/if}
 </main>
+
+
+
 
 <style>
 	main {
